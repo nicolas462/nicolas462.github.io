09bd9512350d44ba5463ea7a9f8d49105496da3c
Nicolás David Espejo Bernal <nicolas462@hotmail.com>
2025-07-06 16:13:00 -0500

    dev branch added

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import photo from "../assets/photo.JPG";
 
 const Header = () => (
   <header className="text-white position-relative text-center">
-  <h1>Nicolas David <br/> Espejo Bernal</h1>
+  <h1>DEV Nicolas David <br/> Espejo Bernal</h1>
   <p>Full Stack Cloud Web Developer</p>
   <div className="profile-image-wrapper">
     <img
